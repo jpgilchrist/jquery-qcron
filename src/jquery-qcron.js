de03bc3261dc36bbc12ddcd4cc6ff5758456178d
James Gilchrist <james.p.gilchrist@gmail.com>
2016-04-13 13:30:11 -0400

    bower install jquery jquery-ui

--- src/jquery-qcron.js
+++ src/jquery-qcron.js
@@ -11,12 +11,23 @@
 
 /* global jQuery:true */
 /* global define:true */
+/* jshint curly:false */
 (function ($) {
     function setup () {
         $.widget("jpgilchrist.qcron", {
-           options: {
-               
-           },
+            options: {
+                initial: "0 0/2 * 1/1 * ? *"
+            },
+            
+            __periods: ["minute(s)", "hour(s)", "day(s)", "week(s)", "month(s)", "year(s)"],
+            __weekdays: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            __months: ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            
+            __tabsTemplate: "<div id='qcron-periods'>" +
+                "<ul>" +
+                "<li><a href='#qcron-minutes'>Minutes</a></li>" +
+                "</ul>" +
+                "<div id='qcron-minutes'>minutes</div></div>",
             
             _create: function (options) {
                 $.extend(this.options, options);
@@ -27,7 +38,35 @@
             },
             
             _init: function () {
-                $(this.element).html("<span>Hello World!</span>");
+                this.$element = $(this.element);
+                this.$element.empty();
+                
+                this.$qcronInputs = $(this.__tabsTemplate).tabs();
+                this.$qcronPreview = $("<div class='qcron-preview'></div>");
+                
+                this.$element.append(this.$qcronInputs).append(this.$qcronPreview);
+                
+                this._renderInputs();
+                this._renderPreview();
+            },
+            
+            _renderInputs: function (expression) {
+//                var self = this;
+//                
+//                if (!!expression)
+//                    expression = this.options.initial;
+//                
+//                this.$period = $("<span>Every <input type='number' min='1'></input> <select></select></span>");
+//                $.each(this.__periods, function (i, period) {
+//                    $("select", self.$period)
+//                        .append($("<option value='" + i + "'>" + period + "</option>"));
+//                });
+                                
+                this.$qcronInputs.append(this.$period);
+            },
+            
+            _renderPreview: function () {
+                this.$qcronPreview.html("<span>preview</span>");
             }
         });
     }
